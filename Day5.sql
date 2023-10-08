f5bbadc97c2050f608164fde004f3a3a261ca51d
surbhi Jain <47022009+jainsurbhii@users.noreply.github.com>
2023-10-09 00:58:06 +0530

    Day5

---
+++ Day5.sql
@@ -0,0 +1,15 @@
+use Emp_Table;
+
+-- INSERT INTO Employees VALUES 
+-- (5,'test5','test5@gmail.com','IT','2022-01-19',56000.00,true),
+-- (6,'test6','test6@gmail.com','EC','2020-07-19',78000.00,false);
+
+
+-- UPDATE Employees
+-- SET Salary = 75000
+-- WHERE ID = 6;
+SELECT * FROM Employees;
+DELETE FROM Employees WHERE ID = 1;
+
+
+
